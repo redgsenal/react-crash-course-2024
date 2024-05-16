5ca95a887c076a587d9ed8655f508e3d9e0c63d6
Reggie <reggie.senal@gmail.com>
2024-05-16 11:49:28 +0800

    update delete

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
-
 const App = () => {
   const addJob = async (newJob) => {
     console.log(newJob);
-    const res = await fetch('/api/jobs', {
-      method: 'POST',
+    const res = await fetch("/api/jobs", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(newJob)
+      body: JSON.stringify(newJob),
     });
-  }
+  };
 
+  // Delete Job
   const deleteJob = async (id) => {
-    console.log(id);
-  }
+    console.log("delete:", id);
+  };
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -34,7 +34,11 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,109 +1,117 @@
-import { useParams, useLoaderData, Link } from "react-router-dom";
+import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 
-const JobPage = () => {
-    const { id } = useParams();
-    const job = useLoaderData();
+const JobPage = ({ deleteJob }) => {
+  const { id } = useParams();
+  const job = useLoaderData();
+  const navigate = useNavigate();
 
-    return (
-        <>
-            {/*<!-- Go Back -->*/}
-            <section>
-                <div className="container m-auto py-6 px-6">
-                    <Link
-                        to="/jobs"
-                        className="text-indigo-500 hover:text-indigo-600 flex items-center"
-                    >
-                        <FaArrowLeft className="mr-2" /> Back to Job Listings
-                    </Link>
+  const onDeleteClick = (jobId) => {
+    console.log(jobId);
+    const confirm = window.confirm("Are you sure to delete?");
+    if (!confirm) {
+      return;
+    }
+    deleteJob(jobId);
+
+    navigate("/jobs");
+  };
+
+  return (
+    <>
+      {/*<!-- Go Back -->*/}
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            to="/jobs"
+            className="text-indigo-500 hover:text-indigo-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
+          </Link>
+        </div>
+      </section>
+
+      <section className="bg-indigo-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <main>
+              <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                <div className="text-gray-500 mb-4">{job.type}</div>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
+                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                  <FaMapMarker className="text-orange-700 mr-1" />
+                  <p className="text-orange-700">{job.location}</p>
                 </div>
-            </section>
+              </div>
 
-            <section className="bg-indigo-50">
-                <div className="container m-auto py-10 px-6">
-                    <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                        <main>
-                            <div
-                                className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
-                            >
-                                <div className="text-gray-500 mb-4">{job.type}</div>
-                                <h1 className="text-3xl font-bold mb-4">
-                                    {job.title}
-                                </h1>
-                                <div
-                                    className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-                                >
-                                   <FaMapMarker className="text-orange-700 mr-1"/>
-                                    <p className="text-orange-700">{job.location}</p>
-                                </div>
-                            </div>
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-indigo-800 text-lg font-bold mb-6">
+                  Job Description
+                </h3>
 
-                            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                                <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                                    Job Description
-                                </h3>
+                <p className="mb-4">{job.description}</p>
 
-                                <p className="mb-4">
-                                    {job.description}
-                                </p>
+                <h3 className="text-indigo-800 text-lg font-bold mb-2">
+                  Salary
+                </h3>
 
-                                <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
+                <p className="mb-4">{job.salary} / Year</p>
+              </div>
+            </main>
 
-                                <p className="mb-4">{job.salary} / Year</p>
-                            </div>
-                        </main>
+            {/*<!-- Sidebar -->*/}
+            <aside>
+              {/*<!-- Company Info -->*/}
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                        {/*<!-- Sidebar -->*/}
-                        <aside>
-                            {/*<!-- Company Info -->*/}
-                            <div className="bg-white p-6 rounded-lg shadow-md">
-                                <h3 className="text-xl font-bold mb-6">Company Info</h3>
+                <h2 className="text-2xl">{job.company.name}</h2>
 
-                                <h2 className="text-2xl">{job.company.name}</h2>
+                <p className="my-2">{job.company.description}</p>
 
-                                <p className="my-2">
-                                    {job.company.description}
-                                </p>
+                <hr className="my-4" />
 
-                                <hr className="my-4" />
+                <h3 className="text-xl">Contact Email:</h3>
 
-                                <h3 className="text-xl">Contact Email:</h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactEmail}
+                </p>
 
-                                <p className="my-2 bg-indigo-100 p-2 font-bold">
-                                    {job.company.contactEmail}
-                                </p>
+                <h3 className="text-xl">Contact Phone:</h3>
 
-                                <h3 className="text-xl">Contact Phone:</h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
+              </div>
 
-                                <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
-                            </div>
-
-                            {/*<!-- Manage -->*/}
-                            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                                <Link
-                                    to={`/job/edit/${job.id}`}
-                                    className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                                >Edit Job
-                                </Link>
-                                <button
-                                    className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                                >
-                                    Delete Job
-                                </button>
-                            </div>
-                        </aside>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
-}
+              {/*<!-- Manage -->*/}
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+                <Link
+                  to={`/job/edit/${job.id}`}
+                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Edit Job
+                </Link>
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Delete Job
+                </button>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 const jobLoader = async ({ params }) => {
-    const res = await fetch(`/api/jobs/${params.id}`);
-    const data = await res.json();
-    return data;
-}
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
 
 export { JobPage as default, jobLoader };
